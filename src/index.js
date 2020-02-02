107e6c6c8d9c272f102eb479b11cbd876be1308e
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-02-02 21:33:02 +0100

    chore(project setup):setup the base project To install express To install all babel packages To install Bodyparser To install nodemon To install dotenv

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// Create global App project
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+  return response.status(200).send({
+    message: 'Welcome to TrackIt app API'
+  });
+});
+
+app.get('/api/v1', (request, response) => {
+  return response.status(200).send({
+    message: 'Welcome to TrackIt api version 1'
+  });
+});
+
+
+app.use((request, response, next) => {
+  const error = new Error('You are trying to access a wrong Route');
+  error.status = 404;
+  next(error);
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  response.status(error.status || 500);
+  response.json({
+    status: error.status || 500,
+    error: error.name,
+    message: error.message
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port: ${PORT}`);
+});
+
+export default app;
